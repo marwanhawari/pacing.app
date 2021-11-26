ec5304eb91c1178204ca5d45a17346a06dc08d53
Marwan Hawari <marwan.hawari@gmail.com>
2021-11-26 00:45:35 +0000

    Updates

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log("pacing.app");
+
+let date;
+date = new Date();
+console.log(date);
+console.log(date.getDay());
+console.log(date);
+console.log(date);
+console.log(date);
+console.log(date.getDate());
+
+let p = document.querySelector("#signature");
+console.log(p.getTotalLength());
+
+let svg = document.querySelector("#svg");
+svg.addEventListener("click", () => {
+    window.open("https://www.github.com/marwanhawari");
+});
