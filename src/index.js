2684d73a83906d37a4d7b58980cc9d8f908560fe
Marwan Hawari <marwan.hawari@gmail.com>
2021-11-28 16:24:45 -0800

    got distance converter working

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ console.log("pacing.app");
 
 let time = document.querySelector(".time");
 let timeChildren = Array.from(time.children);
-// let [timeHours, timeMinutes, timeSeconds] = timeChildren;
 
 let distance = document.querySelector(".distance");
 let [distanceInput, distanceUnit] = distance.children;
-console.log(distanceUnit);
+
+let distanceInputValue = distanceInput.value;
+let distanceUnitValue = distanceUnit.value;
 
 let pace = document.querySelector(".pace");
-let paceChildren = Array.from(pace.children);
-// let [paceHours, paceMinutes, paceSeconds] = paceChildren;
+let paceChildren = Array.from(pace.children).slice(0, -1);
+// let paceUnit = document.querySelector("#pace-unit");
+
+// let paceUnitValue = paceUnit.value;
 
 function divmod(numerator, denominator) {
     return [Math.floor(numerator / denominator), numerator % denominator];
@@ -28,16 +31,53 @@ function convertFromSeconds(totalSeconds) {
 }
 
 function calculatePaceFromTime(time, distance) {
+    console.log(time, distance);
+    // if (updatedDistanceUnit) {
+    //     console.log("converted - pft!");
+    //     distance = convertDistanceUnits(distance, distanceUnitValue);
+    // }
+    console.log(time, distance);
     let totalSeconds = time / distance;
     return convertFromSeconds(totalSeconds);
 }
 
 function calculateTimeFromPace(pace, distance) {
+    // if (updatedDistanceUnit) {
+    //     console.log("converted - tfp!");
+    //     distance = convertDistanceUnits(distance, distanceUnitValue);
+    // }
     let totalSeconds = pace * distance;
+    // console.log(distance);
     return convertFromSeconds(totalSeconds);
 }
 
-function updateDOM(metric, inputChildren, outputChildren, distanceInput) {
+function convertDistanceUnits(value, to) {
+    console.log("to" + to);
+    // if (to == "miles" || to == "/mile") {
+    //     return value * 1.60934;
+    if (to == "kilometers" || to == "/kilometer") {
+        return value * 0.621371;
+    } else {
+        return value;
+    }
+}
+
+// function convertPaceUnits(value, to) {
+//     if (to == "miles") {
+//         return value * 1.60934;
+//     } else if (to == "kilometers") {
+//         return value * 0.621371;
+//     }
+// }
+
+function updateDOM(
+    metric,
+    inputChildren,
+    outputChildren,
+    distanceInputValue,
+    distanceUnitValue,
+    updatedDistanceUnit
+) {
     // Convert the Hr/Min/Sec to total Sec
     let totalConvertedSeconds = convertToSeconds(
         ...inputChildren.map((child) => child.value)
@@ -47,12 +87,16 @@ function updateDOM(metric, inputChildren, outputChildren, distanceInput) {
     if (metric == "pace") {
         calculatedChildren = calculatePaceFromTime(
             totalConvertedSeconds,
-            distanceInput.value
+            distanceInputValue,
+            distanceUnitValue,
+            updatedDistanceUnit
         );
     } else if (metric == "time") {
         calculatedChildren = calculateTimeFromPace(
             totalConvertedSeconds,
-            distanceInput.value
+            distanceInputValue,
+            distanceUnitValue,
+            updatedDistanceUnit
         );
     }
     // Actually modify the values in the DOM
@@ -61,17 +105,107 @@ function updateDOM(metric, inputChildren, outputChildren, distanceInput) {
     }
 }
 
+// document.addEventListener("change", (event) => {
+//     distanceInputValue = distanceInput.value;
+
+//     if (distanceInputValue == 0) {
+//         return;
+//     }
+//     let targetUnitSelector = event.target;
+
+//     if (targetUnitSelector.id == "distance-unit") {
+//         distanceUnitValue = distanceUnit.value;
+//         // console.log(distanceUnitValue);
+//         distanceInputValue = convertDistanceUnits(
+//             distanceInputValue,
+//             distanceUnitValue
+//         );
+//         updateDOM(
+//             "pace",
+//             timeChildren,
+//             paceChildren,
+//             distanceInputValue,
+//             distanceUnitValue
+//         );
+//         console.log("hi");
+//     }
+//     // else if (targetUnitSelector.id == "pace-unit") {
+//     //     let selectedPaceUnit = paceUnit.value;
+//     //     let convertedDistanceValue = convertDistanceUnits(
+//     //         distanceInput.value,
+//     //         selectedPaceUnit
+//     //     );
+//     //     updateDOM("pace", timeChildren, paceChildren, convertedDistanceValue);
+//     // }
+// });
+
 document.addEventListener("input", (event) => {
-    if (distanceInput.value == 0) {
+    distanceInputValue = distanceInput.value;
+    distanceUnitValue = distanceUnit.value;
+
+    console.log("di,du " + [distanceInputValue, distanceUnitValue]);
+
+    if (distanceInputValue == 0) {
         return;
     }
+
+    distanceInputValue = convertDistanceUnits(
+        distanceInputValue,
+        distanceUnitValue
+    );
+
     let targetParentElement = event.target.parentElement;
-    if (
-        targetParentElement.className == "distance" ||
-        targetParentElement.className == "time"
+
+    if (event.target.id == "distance-unit") {
+        distanceUnitValue = distanceUnit.value;
+        // distanceInputValue = convertDistanceUnits(
+        //     distanceInputValue,
+        //     distanceUnitValue
+        // );
+        console.log("du- " + distanceUnitValue);
+        updateDOM(
+            "pace",
+            timeChildren,
+            paceChildren,
+            distanceInputValue,
+            distanceUnitValue,
+            true
+        );
+    } else if (
+        (targetParentElement.className == "distance" ||
+            targetParentElement.className == "time") &&
+        event.target.id != "distance-unit"
     ) {
-        updateDOM("pace", timeChildren, paceChildren, distanceInput);
+        console.log("distance/time- " + distanceUnitValue);
+        updateDOM(
+            "pace",
+            timeChildren,
+            paceChildren,
+            distanceInputValue,
+            distanceUnitValue,
+            false
+        );
     } else if (targetParentElement.className == "pace") {
-        updateDOM("time", paceChildren, timeChildren, distanceInput);
+        console.log("pace- " + distanceUnitValue);
+        updateDOM(
+            "time",
+            paceChildren,
+            timeChildren,
+            distanceInputValue,
+            distanceUnitValue,
+            false
+        );
     }
 });
+
+// distanceUnit.addEventListener("change", (event) => {
+//     if (distanceInput.value == 0) {
+//         return;
+//     }
+//     let selectedUnit = distanceUnit.value;
+//     let convertedDistanceValue = convertDistanceUnits(
+//         distanceInput.value,
+//         selectedUnit
+//     );
+//     updateDOM("pace", timeChildren, paceChildren, convertedDistanceValue);
+// });
--- startup-dev.sh
+++ startup-dev.sh
@@ -2,4 +2,6 @@
 
 # Install the required packages
 npm install
+
+# Set up the pre-commit hooks
 npm run pre-commit
